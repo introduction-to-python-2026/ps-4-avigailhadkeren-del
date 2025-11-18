08ec72a095d7ff611099638a657c9278a8a70857
avigailhadkeren-del <avigail.hadkeren@gmail.com>
2025-11-18 19:26:34 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,4 +3,17 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+  digit_location = 1
+  for char in formula[1:]:
+    if char.isdigit() == True:
+      break
+    digit_location += 1
+
+  if digit_location == len(formula):
+    prefix = formula 
+    numeric = 1
+  else:
+    prefix = formula[:digit_location]
+    numeric = int(formula[digit_location:])
+
+  return (prefix, numeric)
